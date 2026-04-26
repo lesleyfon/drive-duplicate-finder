c267542c44fda7227aa8ef8d0590bd7ba77c3079
lesley <lesleybanadzem2017@gmail.com>
2026-04-26 11:43:58 -0500

    Guard against null accessToken instead of throwing early

--- src/hooks/useStorageQuota.ts
+++ src/hooks/useStorageQuota.ts
@@ -5,13 +5,9 @@ import { getStorageQuota } from "../lib/driveApi";
 export function useStorageQuota() {
 	const { accessToken } = useAuth();
 
-	if (!accessToken) {
-		throw new Error("useStorageQuota must be used within an AuthProvider");
-	}
-
 	return useQuery({
 		queryKey: ["storageQuota"],
-		queryFn: () => getStorageQuota(accessToken),
+		queryFn: () => getStorageQuota(accessToken ?? ""),
 		enabled: !!accessToken,
 		staleTime: 5 * 60 * 1000,
 	});
